726a460254f8d3277f188b9586f2788aaa735798
Oliver Eilhard <oliver.eilhard@gmail.com>
2011-01-29 00:21:39 +0100

    Fix OpenSearch issues

--- lib/rss_feed/open_search.rb
+++ lib/rss_feed/open_search.rb
@@ -12,20 +12,20 @@ module RSSFeed
     end
 
     def total_results
-      @node.at_xpath("rss:rss/rss:channel/opensearch:totalResults", ::RSSFeed::NS).content.to_i rescue nil
+      @node.at_xpath("rss/channel/opensearch:totalResults", ::RSSFeed::NS).content.to_i rescue nil
     end
 
     def start_index
-      @node.at_xpath("rss:rss/rss:channel/opensearch:startIndex", ::RSSFeed::NS).content.to_i rescue nil
+      @node.at_xpath("rss/channel/opensearch:startIndex", ::RSSFeed::NS).content.to_i rescue nil
     end
 
     def items_per_page
-      @node.at_xpath("rss:rss/rss:channel/opensearch:itemsPerPage", ::RSSFeed::NS).content.to_i rescue nil
+      @node.at_xpath("rss/channel/opensearch:itemsPerPage", ::RSSFeed::NS).content.to_i rescue nil
     end
 
     # queries
     def queries
-      nodes = @node.xpath("rss:rss/rss:channel/opensearch:Query", ::RSSFeed::NS) rescue nil
+      nodes = @node.xpath("rss/channel/opensearch:Query", ::RSSFeed::NS) rescue nil
       nodes.map { |node| OpenSearchQuery.new(node) } if nodes
     end
 
